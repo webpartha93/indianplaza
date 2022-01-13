69b1d0423fbe29cb8aa579ceade184a7eb9de7e8
partha <parthapaul199@gmail.com>
2022-01-13 19:06:33 +0530

    unknown item added against to known item

--- config/constant.js
+++ config/constant.js
@@ -1,5 +1,5 @@
 // export const API_URL = 'http://3.106.74.141/metricsapi/public/api';
-export const API_URL = 'https://metricserp.net/metricsapi/public/api';
+// export const API_URL = 'https://metricserp.net/metricsapi/public/api';
 export const API_KEY = '866250ac55306374e9499faa21ce5956';
 export const BEARER_KEY = 'Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJodHRwczpcL1wvbWV0cmljc2VycC5uZXRcL21ldHJpY3NhcGlcL3B1YmxpY1wvYXBpXC9sb2dpbi11c2VyIiwiaWF0IjoxNjE5OTgwMTU1LCJuYmYiOjE2MTk5ODAxNTUsImp0aSI6IlliZnFoajdrRFZwY1AwbloiLCJzdWIiOjU0LCJhY2Nlc3NfdG9rZW4iOm51bGx9.Lh0_WsV8ohTnRIBfy1KM0r1Qvr2xhtypyAXXEb7xjWQ';
 
